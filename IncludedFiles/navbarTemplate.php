32475efb648d09a2306bd3c884ead68b40176834
sewmi <sewmirupasinghe@gmail.com>
2016-09-12 19:56:50 +0530

    check dis one... fonts chnaged and template has been updated.

---
+++ IncludedFiles/navbarTemplate.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>navTemplate</title> 
+  <link href="../assets/bootstrap/bootstrap.css" rel="stylesheet">
+  <link href="../assets/css/navbar1n2.css" rel="stylesheet">
+  <link href="../assets/css/footer.css" rel="stylesheet">
+  <script src="../assets/bootstrap/jquary.js"></script> 
+  <script src="../assets/bootstrap/bootstrapjs.js"></script> 
+</head>
+<body>
+	<!-- start of the heading naavigation bar -->
+  	<nav class="navbar navbar-default navbar1 navbar-li">
+  		<div class="container-fluid"> 
+    	<!-- Brand and toggle get grouped for better mobile display -->
+    		<div class="navbar-header">
+      			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        			<span class="sr-only">Toggle navigation</span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+      			</button>
+      			<a class="navbar-brand" href="#"><span class="header1">Bats</span><span class="header2">Info</span></a>
+    		</div>
+
+    		<!-- Collect the nav links, forms, and other content for toggling -->
+    		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      			<form class="navbar-form  navbar-right">
+        			<div class="form-group">
+          				<input type="text" class="form-control" placeholder="Enter name here">
+        			</div>
+        			<button type="submit" class="btn btn-default">Search</button>
+      			</form>
+      			<ul class="nav navbar-nav  navbar-right navbar-li">
+        			<li class="btn1"><a href="#">Hi Name</a></li>
+        			<li><a href="Help/index.php">Help</a></li> 
+     			</ul> 
+    		</div> <!-- /.navbar-collapse -->
+   		</div><!-- /.container-fluid -->
+ 	</nav>
+	<!-- End of the heading navigation bar -->
+ 
+<!-- start of the side navigation bar -->
+ 	<div>
+ 		<div class="col-xs-2 list-container row">
+ 			<div class="profile-picture">
+ 				<!-- <img src="../img/proPic.png"> -->
+ 			</div>
+			<ul class="nav nav-pills nav-stacked list-of-content">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Add Information</a></li>
+				<li><a href="#">Update Information</a></li>
+				<li><a href="#">Delete Information</a></li>
+				<li><a href="#">Forum</a></li>
+				<li><a href="#">News</a></li>
+				<li><a href="#">About Us</a></li>
+	  		</ul>
+    	</div>
+ 	</div>
+</body>
+</html>
