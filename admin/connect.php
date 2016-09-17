9390236e8f0fa39a771b795b07fd4cc1cf578de1
sewmi <sewmirupasinghe@gmail.com>
2016-09-17 22:52:42 +0530

    same

--- admin/connect.php
+++ admin/connect.php
@@ -5,5 +5,5 @@ $mysql_password = "";
 $mysql_database = "project";
 $prefix = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysql_select_db($mysql_database, $bd) or die("Could not select database");
+mysql_select_db( $mysql_database, $bd ) or die("Could not select database");
 ?>
