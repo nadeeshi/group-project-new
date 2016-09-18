3558e89340095284581ff566f7c3c6304559951c
sewmi <sewmirupasinghe@gmail.com>
2016-09-18 12:10:12 +0530

    fonts edited in navbar

--- mainhome/dbconnect.php
+++ mainhome/dbconnect.php
@@ -1,4 +1,4 @@
 <?php
 //connect to mysql database
-$con = mysqli_connect("localhost", "root", "", "register") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("localhost", "root", "", "project") or die("Error " . mysqli_error($con));
 ?>
