e4e6f5adcbb5e3ed923be47d0c1aad7fb01efbab
sewmi <sewmirupasinghe@gmail.com>
2016-10-28 05:50:09 +0530

    forum updated

--- forum/addThread.php
+++ forum/addThread.php
@@ -1,3 +1,24 @@
+<?php
+$msg='';
+if (!empty($_POST)){
+require_once ("connection.php");
+$topic=$_POST['topic'];
+$massage=$_POST['massage'];
+/*$postby=$_POST[]*/
+$datetime=date("d/m/y h:i:s"); 
+
+
+$query= "INSERT INTO topics (topic_subject,topic_content,topic_date) VALUES ('$topic', '$massage', '$datetime')";
+$result= mysqli_query($db, $query);
+if ($result) {
+  $msg='Added Succesfully';
+}
+else {
+  $msg="Error" . "<br>". mysqli_error($db);
+}
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,6 +29,8 @@
     <link href="../assets/css/footer.css" rel="stylesheet">
     <script src="../assets/bootstrap/jquary.js"></script> 
     <script src="../assets/bootstrap/bootstrapjs.js"></script>
+    <script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
   </head>
   <body>
     <div>
@@ -16,29 +39,30 @@
     <div class="col-sm-8 col-sm-push-2 col-xs-12 insert-form">
     	<div class="content col-xs-12">
         <h4 class="header-content">Your New Discussion Topic</h4>
-          <form class="form-horizontal form-group-custom">
+          <form class="form-horizontal form-group-custom" method="post" action="addThread.php">
             <div class="form-group">
-              <label class="control-label col-xs-2" for="email">Subject:</label>
+              <label class="control-label col-xs-2" for="subject">Subject:</label>
               <div class="col-xs-8">
-                <input type="email" class="form-control form-row" id="email" placeholder="Enter Subject">
+                <input type="text" class="form-control form-row" id="subject" name="topic" placeholder="Enter Subject">
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-xs-2" for="pwd">Massage:</label>
               <div class="col-xs-8">
-                <textarea class="massage form-row form-control" rows="6" cols="8">Enter Massage Here</textarea>
+                <textarea class="massage form-row form-control" rows="6" cols="8" name="massage">Enter Massage Here</textarea>
               </div>
             </div>
             <div class="form-group">
               <label class="control-label col-xs-2" for="pwd">Attachment:</label>
               <div class="col-xs-8">
-                <input type="file" >
+                <input type="file" name="files" >
               </div>
             </div>
             <div class="form-group form-group-custom">
               <div class="col-xs-offset-2 col-xs-8 btn-class">
                 <button type="submit" class="btn btn-default button-custom">Post to forum</button>
               </div>
+              <?php echo $msg; ?>
             </div>
           </form>
       </div>
