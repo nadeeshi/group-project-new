759654fbdcf096ae512e81c1aad66545767fb32a
macnmunasinghe <munasinghenuwan@yahoo.com>
2016-10-30 10:00:24 +0530

    added template to map and news
    removed unwanted css files

--- batnews/news_main - Copy.php
+++ batnews/news_main - Copy.php
@@ -2,37 +2,118 @@
 <html>
 <head>
   <title>batsinfo</title> 
-  <link href="https://fonts.googleapis.com/css?family=Alike+Angular" rel="stylesheet"> 
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/navnew.css" rel="stylesheet">
+  <link href="css/footer.css" rel="stylesheet">
+  
+  
+  <link rel="stylesheet" type="text/css" href="css/z/CS2.css">
+  
+  <script src="js/jquary.js"></script> 
+  <script src="js/bootstrapjs.js"></script> 
+  
+  <style>
 	
-	<link href="../assets/bootstrap/bootstrap.css" rel="stylesheet" type="text/css">
-	<link href="../assets/css/newsmain.css" rel="stylesheet" type="text/css">	
-	<link href="../assets/css/navbar1n2.css" rel="stylesheet" type="text/css">
-	<link href="../assets/css/footer.css" rel="stylesheet">
-	<script src="../assets/bootstrap/jquary.js"></script> 
-  	<script src="../assets/bootstrap/bootstrapjs.js"></script>
-	
-	
-	
+.footer-container {
+	z-index:0.9;
+	position: absolute;
+    color: #ffffff;
+    background-color: #4c4743;
+    margin-bottom: 0px;
+    margin-left: 15px;
+	margin-right: 100px;
+    font-family: 'Varela Round', sans-serif;
+    margin-top: -7px  !important;
+    padding-bottom: 39px;
+	width:100%;	
+		}
+	.footcon-col-1 {
+    margin-top: 10px;
+    margin-left: 10px;
+}
+</style>
+  
 </head>
 <body>
-	<div>
-		<?php include ("../IncludedFiles/navbarTemplate.php"); ?>
-	</div>
-	<div class="col-sm-8 col-sm-push-2 col-xs-12 insert-form">
+	<!-- start of the heading naavigation bar -->
+  <!-- <div class="nav-header"> -->
+  	<nav class="navbar navbar-default navbar1 navbar-li">
+  		<div class="container-fluid"> 
+    	<!-- Brand and toggle get grouped for better mobile display -->
+    		<div class="navbar-header">
+      			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        			<span class="sr-only">Toggle navigation</span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+      			</button>
+      			<a class="navbar-brand" href="#" style="font-family:'Chewy', cursive  !important;
+  font-size: 50px;"><span class="header1" >Bats</span><span class="header2">Info</span></a>
+    		</div>
+
+    		<!-- Collect the nav links, forms, and other content for toggling -->
+        
+    		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      			<form class="navbar-form  navbar-right" action="../batmap/map_and_det.php" method="post">
+        			<div class="form-group">
+          				<input type="text" class="form-control input-area" name='address' required placeholder="Enter name here">
+        			</div>
+        			<button type="submit" class="btn btn-default">Search</button>
+      			</form>
+      			<ul class="nav navbar-nav  navbar-right navbar-li">
+        			<li class="btn1"><a href="#">Hi Name</a></li> 
+              
+              <li class="min-link"><a href="../Help/index.php">Help</a></li>
+              
+     			</ul> 
+    		</div> <!-- /.navbar-collapse -->
+      </div>
+   	<!-- /.container-fluid -->
+ 	  </nav>
+	<!-- End of the heading navigation bar -->
+ 
+  <div class="row"> 
+  
+    <!-- start of the side navigation bar -->
+ 		<div class="col-xs-2 list-container mini-bar">
+ 			<div class="profile-picture">
+ 				<!-- <img src="../img/proPic.png"> -->
+ 			</div>
+			<ul class="nav nav-pills nav-stacked list-of-content">
+				<li><a href="../phpPages/researcherHomePage.php">Home</a></li>
+				<li><a href="../final_insert_update_del/insert.php">Add Information</a></li>
+				<li><a href="../final_insert_update_del/update_bat.php">Update Information</a></li>
+				<li><a href="../final_insert_update_del/delete_bat.php">Delete Information</a></li>
+				<li><a href="../forum/forum.php">Forum</a></li>
+				<li><a href="news_main.php">News</a></li>
+        <li><a href="../Help/index.php">Help</a></li>
+				<li><a href="#">About Us</a></li>
+			</ul>
+		</div>
+    <!-- end of the side navigation bar -->
+    
+
+
+
+      <!-- me div eka danna dana hema ekakatama -->
+
+
+		<div class="col-sm-8 col-sm-push-2 col-xs-12 insert-form">
 		<div class="container">
 	
 	<?php 
 					$mysql_hostname = "localhost";
 					$mysql_user = "root";
 					$mysql_password = "";
-					$mysql_database = "test2";
+					$mysql_database = "project";
 					$con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database );
 					$qry = "SELECT * FROM photos ORDER BY id DESC;";
 					$imgList =array(1,2,3,4);
 					$desc = array(1,2,3,4);
 					$caption =array(1,2,3,4);
 					$id = array(1,2,3,4);
-					$result = mysqli_query($con, $qry) or die();
+					$result = mysqli_query($con, $qry);
 					if (mysqli_num_rows($result) >= 4){
 						for($i = 0 ; $i < 4;$i++){
 							mysqli_data_seek($result,$i);
@@ -260,19 +341,30 @@
 	</div>
 
 	</div>
-	
 	</div>
+<div class="row">
+		<div class="col-xs-12 footer-container">
+			<div class="row">
+				<div class="col-xs-push-2 col-xs-4 footcon-col-1">
+					<p>Mail Us:</p>
+					<p>Faculty of Science<br>University of Colombo<br>Colombo 07<br>Sri Lanka</p>
+				</div>
+				<div class="col-xs-push-2 col-xs-4 footcon-col-1">
+					<p>Call Us:</p>
+					<p>+94116250036</p>
+				</div>
+				<div class="col-xs-push-1 col-xs-3 footcon-col-1">
+					<p>Mail Us:</p>
+					<p>zoologydept@fos.cmb.ac.lk</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-push-10 col-xs-2">
+					<p>All Rights Reserved</p>
+				</div>
+			</div>
+		</div>
 
-
-	<!-- start footer -->
-
-	<div class="row">
-	    <div class="col-xs-10 col-xs-push-2">
-	      <?php include ("../IncludedFiles/footer.php"); ?>
-	    </div>  
-  	</div>
-
-
-	<!-- end of footer -->
+</div>	
 </body>
 </html>
