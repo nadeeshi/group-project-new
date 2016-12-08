fdbcc17c214028ac1253f4a383cf19a9abcb25fb
macnmunasinghe <munasinghenuwan@yahoo.com>
2016-12-08 14:45:11 +0530

    rm

--- news_insert/x/connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host="localhost";
-$user="root";
-$pass="";
-//connection
-$con=mysql_connect($host,$user,$pass) or die(mysql_error());
-//db selection
-mysql_select_db("test2",$con);
-?>
--- news_insert/x/delete.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include('connection.php');
-$sql=mysql_query("delete from users where email='{$_GET['email']}'");
-unlink("image/".$_GET['email']."/".$_GET['image']);
-rmdir("image/".$_GET['email']);
-header('location:index.php');
-?>
