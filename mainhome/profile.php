b7a389950e1ae31287bd61f4e7c6e04859112ad1
Ahmed Rooshan <mhaarush@gmail.com>
2017-01-09 12:16:17 +0530

    user profile view
    on progress

---
+++ mainhome/profile.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>nav1</title>
+  <link href="../assets/bootstrap/bootstrap.css" rel="stylesheet">
+  <link href="../assets/css/navbar1n2.css" rel="stylesheet">
+  <link href="../assets/css/phpPages.css" rel="stylesheet">
+  <link href="../assets/css/footer.css" rel="stylesheet">
+  <script src="../assets/bootstrap/jquary.js"></script> 
+  <script src="../assets/bootstrap/bootstrapjs.js"></script> 
+  <script src="../js/jssor.slider.mini.js"></script>
+
+   
+    
+</head>
+
+<body>
+
+    <div>
+        <?php include '../IncludedFiles/navbarTemplate.php'; ?>
+    </div>
+
+    <div class="container-fluid" style="padding-top:120px;">
+    <div class="col-sm-8 col-sm-push-4 col-xs-12">
+	   <h2> Your Profile  </h2>
+       <br>
+       <div id = "info">
+            <table border="0" class="table table-hover">
+                <tr>
+                    <td>Profile ID</td>
+                    <td><input type="text" name="profileid" value="" disabled=""></td>
+                </tr>
+                <tr>
+                    <td>First Name</td>
+                    <td><input type="text" name="profileid" value=""></td>
+                </tr>
+                <tr>
+                    <td>Last Name</td>
+                    <td><input type="text" name="profileid" value=""></td>
+                </tr>
+                <tr>
+                    <td>NIC Number</td>
+                    <td><input type="text" name="profileid" value="" disabled=""></td>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                    <td><input type="text" name="profileid" value="" disabled=""></td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td><input type="text" name="profileid" value="" disabled=""></td>
+                </tr>
+                <tr>
+                    <td>Country</td>
+                    <td><input type="text" name="profileid" value="" disabled=""></td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td><input type="text" name="profileid" value=""></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td><input type="text" name="profileid" value="" disabled=""></td>
+                </tr>
+            </table>
+       </div>
+	  </div> 
+    <div class="push"></push>
+	</div>
+
+</body>
+</html>
